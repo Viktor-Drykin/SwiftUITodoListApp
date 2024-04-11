74d0b43112effa5c01013b303b8e2fa28949dd0b
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-04-11 17:31:42 +0100

    init project

---
+++ TodoListSwiftUI/TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by DataArt Viktor Drykin on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
